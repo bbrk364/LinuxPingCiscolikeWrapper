d2ea24414ce7ac4c98b1db4f139ee141b93a97a7
igoroy <igoroy@users.noreply.github.com>
2015-02-04 09:47:44 +0200

    It turned out that "(truncated)" message is not in sequence sync.

--- ciscolikeping.sh
+++ ciscolikeping.sh
@@ -18,9 +18,9 @@
 #From 212.1.97.206 icmp_seq=1 Time to live exceeded
 #Request timeout for icmp_seq 159	<-- This is from BSD
 # Assuming that sequence conter is in sync
-#72 bytes from 172.26.22.10: icmp_seq=2 ttl=251 (truncated)
 #From 192.168.0.52: icmp_seq=9 Source Quench
 # Sequence counter is not in sync
+#72 bytes from 172.26.22.10: icmp_seq=2 ttl=251 (truncated)
 #From 10.0.0.4: icmp_seq=1 Redirect Host(New nexthop: 10.0.0.17)
 #From 10.0.0.17: icmp_seq=2 Redirect Network(New nexthop: 10.0.0.23)
 #From 195.137.185.66 icmp_seq=2 Frag needed and DF set (mtu = 1460)
@@ -58,9 +58,9 @@ fil=$(expr "$line" : 'From .* icmp_[rs]eq=\([0-9]\{1,\}\) Packet filtered$')
 ttl=$(expr "$line" : 'From .* icmp_[rs]eq=\([0-9]\{1,\}\) Time to live exceeded$')
 tmo=$(expr "$line" : 'Request timeout for icmp_seq \([0-9]\{1,\}\)$')
 # Assuming that sequence conter is in sync
-trc=$(expr "$line" : '[0-9]\{1,\} bytes from .*: icmp_[rs]eq=\([0-9]\{1,\}\) ttl=[0-9]\{1,\} (truncated)$')
 qnc=$(expr "$line" : 'From .*: icmp_[rs]eq=\([0-9]\{1,\}\) Source Quench$')
 # Sequence counter is not in sync
+trc=$(expr "$line" : '[0-9]\{1,\} bytes from .*: icmp_[rs]eq=\([0-9]\{1,\}\) ttl=[0-9]\{1,\} (truncated)$')
 hst=$(expr "$line" : 'From .*: icmp_[rs]eq=\([0-9]\{1,\}\) Redirect Host(New nexthop: .*)$')
 net=$(expr "$line" : 'From .*: icmp_[rs]eq=\([0-9]\{1,\}\) Redirect Network(New nexthop: .*)$')
 fra=$(expr "$line" : 'From .* icmp_[rs]eq=\([0-9]\{1,\}\) Frag needed and DF set (mtu = .*)$')
@@ -162,29 +162,6 @@ if [[ "$ttl" ]]
     if [[ "$ttl" -eq "$n" ]]
       then
         echo -n "x"
-    fi
-    if (( count % 80 == 0 )); then echo " $count"; fi
-    (( n++ ))
-    (( count++ ))
-fi
-
-# Echo reply is truncated
-if [[ "$trc" ]]
-  then
-    if [[ "$trc" -gt "$n" ]]
-      then
-        dif=$(( trc - n ))	# let "dif=$trc-$n"
-        for ((a=1; a <= dif ; a++))
-          do
-            echo -n "."
-            if (( count % 80 == 0 )); then echo " $count"; fi
-            (( n++ ))
-            (( count++ ))
-          done
-    fi
-    if [[ "$trc" -eq "$n" ]]
-      then
-        echo -n "t"
     fi
     if (( count % 80 == 0 )); then echo " $count"; fi
     (( n++ ))
